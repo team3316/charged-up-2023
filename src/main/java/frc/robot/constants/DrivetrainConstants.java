58e079b16b9f5f0e763a0569e3dfe1cd9617079a
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-01-08 17:52:20 +0200

    added reminder to fix constants

--- src/main/java/frc/robot/constants/DrivetrainConstants.java
+++ src/main/java/frc/robot/constants/DrivetrainConstants.java
@@ -61,14 +61,17 @@ public class DrivetrainConstants {
         }
     }
 
+    // TODO: update robot dimensions
     public static final double frontWheelDistMeters = 0.6703;
     public static final double sideWheelDistMeters = 0.5102;
 
+    // TODO: update cancoder offsets
     public final static double cancoderTLOffset = 14.0;// 10.1;
     public final static double cancoderTROffset = 65.4;// 243 - 180;// 234.3;
     public final static double cancoderBLOffset = 92.8;// 109.6;
     public final static double cancoderBROffset = 159.6;// 159.3;
 
+    // TODO: update CAN IDs
     public static final SwerveModuleConstants TLModule = new SwerveModuleConstants(
             new Translation2d(-frontWheelDistMeters / 2, sideWheelDistMeters / 2), 3, 4,
             cancoderTLOffset, 11);
