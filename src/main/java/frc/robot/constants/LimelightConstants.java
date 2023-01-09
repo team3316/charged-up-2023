ca8e8d753e828ed4fd587174c30f85ddebc5fb3b
jizelda <70440778+jizelda@users.noreply.github.com>
2023-01-09 19:55:02 +0200

    added limelight subsystem

---
+++ src/main/java/frc/robot/constants/LimelightConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+/**
+ * LimelightConstants
+ */
+public class LimelightConstants {
+  public static final double limelightHeightMeters = 0;
+  public static final double lowGoalTargetHeightMeters = 0;
+  public static final double highGoalTargetHeightMeters = 0;
+  
+}
---
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import org.opencv.core.Point;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.LimelightConstants;
+
+public class LimeLight extends SubsystemBase {
+    NetworkTable limeLightTable;
+    private NetworkTableEntry ta;
+    private NetworkTableEntry tx;
+    private NetworkTableEntry ty;
+
+    /** Creates a new LimeLight. */
+    public LimeLight() {
+        limeLightTable = NetworkTableInstance.getDefault().getTable("limelight");
+        tx = limeLightTable.getEntry("tx");
+        ty = limeLightTable.getEntry("ty");
+        ta = limeLightTable.getEntry("ta");
+    }
+
+    public double getAngle() {
+      return tx.getDouble(0);
+    }
+
+    //height dif between goal and limelight devided by tan of limelight y angle.
+    public double getDist() {
+      return (LimelightConstants.lowGoalTargetHeightMeters-LimelightConstants.limelightHeightMeters)
+      /Math.tan(Math.toRadians(ty.getDouble(0)));
+    }
+
+    
+
+}
