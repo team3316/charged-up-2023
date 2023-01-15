ed06e76fbb3ce9f4a333a6ffde27f010fe857ffb
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-01-15 19:13:03 +0200

    corrected funnel constants to be final

--- src/main/java/frc/robot/constants/FunnelConstants.java
+++ src/main/java/frc/robot/constants/FunnelConstants.java
@@ -3,12 +3,12 @@ package frc.robot.constants;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class FunnelConstants {
-    public static DoubleSolenoid.Value collectState = DoubleSolenoid.Value.kForward;
-    public static DoubleSolenoid.Value installState = DoubleSolenoid.Value.kForward;
-    public static DoubleSolenoid.Value closedState = DoubleSolenoid.Value.kReverse;
+    public static final DoubleSolenoid.Value collectState = DoubleSolenoid.Value.kForward;
+    public static final DoubleSolenoid.Value installState = DoubleSolenoid.Value.kForward;
+    public static final DoubleSolenoid.Value closedState = DoubleSolenoid.Value.kReverse;
 
-    public static double collectSpeed = -1;
-    public static double installSpeed = 0;
-    public static double closedSpeed = 0;
+    public static final double collectSpeed = -1;
+    public static final double installSpeed = 0;
+    public static final double closedSpeed = 0;
 
 }
