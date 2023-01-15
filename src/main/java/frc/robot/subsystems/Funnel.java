78c2237e1dd5accf9b180129a822880ee0fff378
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-01-15 18:03:43 +0200

    added set funnel command

--- src/main/java/frc/robot/subsystems/Funnel.java
+++ src/main/java/frc/robot/subsystems/Funnel.java
@@ -11,7 +11,10 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
 import frc.robot.constants.FunnelConstants;
 
 public class Funnel extends SubsystemBase {
@@ -45,7 +48,7 @@ public class Funnel extends SubsystemBase {
     
   }
   
-  public void setFunnelState( FunnelState state){
+  public void setFunnelState(FunnelState state){
     if(state == getFunnelState()){
       return;
     }
@@ -60,5 +63,7 @@ public class Funnel extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  
+  public CommandBase setFunnelStateCommand(FunnelState state) {
+    return new InstantCommand(() -> {setFunnelState(state);},this);
+  }
 }
