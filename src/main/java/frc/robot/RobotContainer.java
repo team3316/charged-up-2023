4ae6b9ff8577ebc24c07237481d7e462e685d174
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-01-16 20:16:01 +0200

    Added code in RobotContainer for testing commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -11,6 +13,7 @@ import frc.robot.constants.DrivetrainConstants;
 import frc.robot.constants.DrivetrainConstants.SwerveModuleConstants;
 import frc.robot.constants.JoysticksConstants;
 import frc.robot.humanIO.CommandPS5Controller;
+import frc.robot.subsystems.Manipulator;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
 /**
@@ -18,47 +21,51 @@ import frc.robot.subsystems.drivetrain.Drivetrain;
  * commands, and trigger mappings).
  */
 public class RobotContainer {
-    private final Drivetrain m_drivetrain = new Drivetrain();
+  private final Drivetrain m_drivetrain = new Drivetrain();
+  private final Manipulator m_Manipulator = new Manipulator();
+  private final Compressor m_compressor = new Compressor(PneumaticsModuleType.REVPH);
 
-    private final CommandPS5Controller _driverController = new CommandPS5Controller(
-            JoysticksConstants.driverPort);
+  private final CommandPS5Controller _driverController = new CommandPS5Controller(
+      JoysticksConstants.driverPort);
 
-    private boolean _fieldRelative = true;
+  private boolean _fieldRelative = true;
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        // Configure the trigger bindings
-        configureBindings();
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    m_compressor.enableDigital();
 
-        m_drivetrain.setDefaultCommand(
-                new RunCommand(
-                        () -> m_drivetrain.drive(
-                                _driverController.getLeftY() * SwerveModuleConstants.freeSpeedMetersPerSecond,
-                                _driverController.getLeftX() * SwerveModuleConstants.freeSpeedMetersPerSecond,
-                                _driverController.getCombinedAxis() * DrivetrainConstants.maxRotationSpeedRadPerSec,
-                                _fieldRelative),
-                        m_drivetrain));
-    }
+    // Configure the trigger bindings
+    configureBindings();
 
-    /**
-     * Use this method to define your trigger->command mappings.
-     */
-    private void configureBindings() {
-        _driverController.options().onTrue(
-                new InstantCommand(() -> _fieldRelative = !_fieldRelative)); // toggle field relative mode
+    m_drivetrain.setDefaultCommand(
+        new RunCommand(
+            () -> m_drivetrain.drive(
+                _driverController.getLeftY() * SwerveModuleConstants.freeSpeedMetersPerSecond,
+                _driverController.getLeftX() * SwerveModuleConstants.freeSpeedMetersPerSecond,
+                _driverController.getCombinedAxis() * DrivetrainConstants.maxRotationSpeedRadPerSec,
+                _fieldRelative),
+            m_drivetrain));
+  }
 
-        _driverController.share().onTrue(
-                new InstantCommand(m_drivetrain::resetYaw)); // toggle field relative mode
-    }
+  /**
+   * Use this method to define your trigger->command mappings.
+   */
+  private void configureBindings() {
+    _driverController.options().onTrue(
+        new InstantCommand(() -> _fieldRelative = !_fieldRelative)); // toggle field relative mode
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        return new InstantCommand();
-    }
+    _driverController.share().onTrue(
+        new InstantCommand(m_drivetrain::resetYaw)); // toggle field relative mode
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return new InstantCommand();
+  }
 }
