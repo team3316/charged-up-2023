e1b2898d4d593252b376e8197357256f0fa4bfa4
Takamagahara <yoav2905@gmail.com>
2023-01-17 21:49:34 +0200

    Revert "Added manipulator subsystem"
    This reverts commit f71786bea00bf74e443b90d98d032880973d9c46.

--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -4,74 +4,23 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.ManipulatorConstants;
 
 public class Manipulator extends SubsystemBase {
+  
 
-    private ManipulatorState _currentState;
-    private DigitalInput _gamePieceDetector;
+  public enum State {
+    OPEN,
+    CUBE_HOLD,
+    CONE_HOLD,
+    OFF;
+  }
+  
+  /** Creates a new Manipulator. */
+  public Manipulator() {}
 
-    private DoubleSolenoid _leftSolenoid;
-    private DoubleSolenoid _rightSolenoid;
-
-    public enum ManipulatorState {
-        OPEN(ManipulatorConstants.leftSolenoidOpenState, ManipulatorConstants.rightSolenoidOpenState),
-        CUBE_HOLD(ManipulatorConstants.leftSolenoidCubeClosedState, ManipulatorConstants.rightSolenoidCubeClosedState),
-        CONE_HOLD(ManipulatorConstants.leftSolenoidConeClosedState, ManipulatorConstants.rightSolenoidConeClosedState);
-
-        public final DoubleSolenoid.Value leftSolenoidState;
-        public final DoubleSolenoid.Value rightSolenoidState;
-
-        private ManipulatorState(DoubleSolenoid.Value LeftSolenoidState, DoubleSolenoid.Value RightSolenoidState) {
-            this.leftSolenoidState = LeftSolenoidState;
-            this.rightSolenoidState = RightSolenoidState;
-        }
-    }
-
-    /** Creates a new Manipulator. */
-    public Manipulator() {
-        this._gamePieceDetector = new DigitalInput(ManipulatorConstants.sensorID);
-        this._leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH,
-                ManipulatorConstants.leftSolenoidForwardChannel,
-                ManipulatorConstants.leftSolenoidReverseChannel);
-        this._rightSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH,
-                ManipulatorConstants.rightSolenoidForwardChannel,
-                ManipulatorConstants.rightSolenoidReverseChannel);
-    }
-
-    public ManipulatorState getManipulatorState() {
-        return this._currentState;
-    }
-
-    public void setManipulatorState(ManipulatorState requiredState) {
-        if (this.getManipulatorState() == requiredState) {
-            return;
-        }
-
-        this._leftSolenoid.set(requiredState.leftSolenoidState);
-        this._rightSolenoid.set(requiredState.rightSolenoidState);
-
-        this._currentState = requiredState;
-    }
-
-    public boolean isHoldingGamePiece() {
-        return this._gamePieceDetector.get();
-    }
-
-    @Override
-    public void periodic() {
-
-    }
-
-    public CommandBase setFunnelStateCommand(ManipulatorState state) {
-        return new InstantCommand(() -> {
-            setManipulatorState(state);
-        }, this);
-    }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
