98917a1040cc075d93c0125becc282899b581f33
Takamagahara <yoav2905@gmail.com>
2023-01-17 21:49:31 +0200

    Revert "changed ports"
    This reverts commit 0682b3d1f8d23f2bc79085cfea45d5aa5e6b63cc.

--- src/main/java/frc/robot/constants/ManipulatorConstants.java
+++ src/main/java/frc/robot/constants/ManipulatorConstants.java
@@ -6,16 +6,16 @@ public class ManipulatorConstants {
 
     public static final int sensorID = 0;
 
-    public static final int leftSolenoidForwardChannel = 0;
-    public static final int leftSolenoidReverseChannel = 1;
-    public static final int rightSolenoidForwardChannel = 2;
-    public static final int rightSolenoidReverseChannel = 3;
+    public static final int leftSolenoidForwardChannel = 1;
+    public static final int leftSolenoidReverseChannel = 2;
+    public static final int rightSolenoidForwardChannel = 3;
+    public static final int rightSolenoidReverseChannel = 4;
 
     public static final DoubleSolenoid.Value leftSolenoidOpenState = DoubleSolenoid.Value.kReverse;
     public static final DoubleSolenoid.Value rightSolenoidOpenState = DoubleSolenoid.Value.kReverse;
     public static final DoubleSolenoid.Value leftSolenoidCubeClosedState = DoubleSolenoid.Value.kForward;
-    public static final DoubleSolenoid.Value rightSolenoidCubeClosedState = DoubleSolenoid.Value.kReverse;
-    public static final DoubleSolenoid.Value leftSolenoidConeClosedState = DoubleSolenoid.Value.kReverse;
+    public static final DoubleSolenoid.Value rightSolenoidCubeClosedState = DoubleSolenoid.Value.kOff;
+    public static final DoubleSolenoid.Value leftSolenoidConeClosedState = DoubleSolenoid.Value.kOff;
     public static final DoubleSolenoid.Value rightSolenoidConeClosedState = DoubleSolenoid.Value.kForward;
 
 }
