2b267f0ee10e2ad3d22365d2767887aa71fde770
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-01-19 17:05:27 +0200

    added printing to Smart dashboard

--- src/main/java/frc/robot/subsystems/Funnel.java
+++ src/main/java/frc/robot/subsystems/Funnel.java
@@ -10,6 +10,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -59,8 +60,17 @@ public class Funnel extends SubsystemBase {
   }
   @Override
   public void periodic() {
+    updateSDB();
     // This method will be called once per scheduler run
   }
+
+  @SuppressWarnings({ "unused" })
+    private void updateSDB() {
+        SmartDashboard.putString("funnel state", this._currentState.toString());
+        SmartDashboard.putNumber("funnel roller speed", this._rollers.get());
+        
+    }
+
 
   public CommandBase setFunnelStateCommand(FunnelState state) {
     return new InstantCommand(() -> {setFunnelState(state);},this);
