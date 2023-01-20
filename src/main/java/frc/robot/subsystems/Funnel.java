d4358923ce64ef4136f4a2407bedb310c85f32c4
gurilad <giladbh21@gmail.com>
2023-01-20 13:15:01 +0200

    add use of DBugSparkMax

--- src/main/java/frc/robot/subsystems/Funnel.java
+++ src/main/java/frc/robot/subsystems/Funnel.java
@@ -5,7 +5,6 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -15,54 +14,55 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.FunnelConstants;
+import frc.robot.motors.DBugSparkMax;
+import frc.robot.motors.PIDFGains;
 
 public class Funnel extends SubsystemBase {
 
-    private CANSparkMax _rollers;
-    private FunnelState _currentState;
-    private DoubleSolenoid _funnelSolenoid;
+  private CANSparkMax _rollers;
+  private FunnelState _currentState;
+  private DoubleSolenoid _funnelSolenoid;
 
-    public enum FunnelState {
-        COLLECT(FunnelConstants.openState, FunnelConstants.collectPercent),
-        INSTALL(FunnelConstants.openState, FunnelConstants.installPercent),
-        CLOSED(FunnelConstants.closedState, FunnelConstants.closedPercent);
+  public enum FunnelState {
+    COLLECT(FunnelConstants.openState, FunnelConstants.collectPercent),
+    INSTALL(FunnelConstants.openState, FunnelConstants.installPercent),
+    CLOSED(FunnelConstants.closedState, FunnelConstants.closedPercent);
 
-        public final DoubleSolenoid.Value solenoidState;
-        public final double rollerPercent;
+    public final DoubleSolenoid.Value solenoidState;
+    public final double rollerPercent;
 
-        private FunnelState(Value solenoidState, double rollerPercent) {
-            this.solenoidState = solenoidState;
-            this.rollerPercent = rollerPercent;
-        }
-    };
+    private FunnelState(Value solenoidState, double rollerPercent) {
+      this.solenoidState = solenoidState;
+      this.rollerPercent = rollerPercent;
+    }
+  };
 
-    public Funnel() {
-        this._funnelSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, FunnelConstants.solenoidForwardPort,
-                FunnelConstants.solenoidReversePort);
-        this._rollers = new CANSparkMax(FunnelConstants.sparkMaxPort, MotorType.kBrushless);
-        this._rollers.restoreFactoryDefaults();
+  public Funnel() {
+    this._funnelSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, FunnelConstants.solenoidForwardPort,
+        FunnelConstants.solenoidReversePort);
 
+    // We only use this SparkMax with Percent Output. So no need for all params
+    this._rollers = DBugSparkMax.create(FunnelConstants.sparkMaxPort, new PIDFGains(0), 1, 1, 0);
+  }
+
+  public FunnelState getFunnelState() {
+    return _currentState;
+  }
+
+  public void setFunnelState(FunnelState state) {
+    if (state == getFunnelState()) {
+      return;
     }
 
-    public FunnelState getFunnelState() {
-        return _currentState;
-    }
+    _funnelSolenoid.set(state.solenoidState);
+    _rollers.set(state.rollerPercent);
 
-    public void setFunnelState(FunnelState state) {
-        if (state == getFunnelState()) {
-            return;
-        }
+    _currentState = state;
 
-        _funnelSolenoid.set(state.solenoidState);
-        _rollers.set(state.rollerPercent);
+    SmartDashboard.putString("Funnel State", this._currentState.toString());
+  }
 
-        _currentState = state;
-
-        SmartDashboard.putString("Funnel State", this._currentState.toString());
-    }
-
-
-    public CommandBase setFunnelStateCommand(FunnelState state) {
-        return new InstantCommand(() -> setFunnelState(state), this);
-    }
+  public CommandBase setFunnelStateCommand(FunnelState state) {
+    return new InstantCommand(() -> setFunnelState(state), this);
+  }
 }
