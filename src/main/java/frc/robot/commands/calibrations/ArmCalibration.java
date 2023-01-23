c36c02d99d7bde688bfc3d10de1ec3be9d4cfdb8
--global <51059131+DarkMetalMouse@users.noreply.github.com>
2023-01-23 17:45:18 +0200

    added pid and feedforward calibrations

--- src/main/java/frc/robot/commands/calibrations/ArmCalibration.java
+++ src/main/java/frc/robot/commands/calibrations/ArmCalibration.java
@@ -2,12 +2,14 @@ package frc.robot.commands.calibrations;
 
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.Arm;
 
@@ -19,6 +21,18 @@ public class ArmCalibration extends CommandBase {
 
     Subsystem _subsystem;
     TalonFX _leader;
+    TalonFXConfiguration _leaderConfig;
+
+    // ff gains
+    double ks = 0;
+    double kv = 0;
+    double kg = 0;
+    double ka = 0;
+
+    // pid gains
+    double kp = 0;
+    double ki = 0;
+    double kd = 0;
 
     boolean isEnded = false;
 
@@ -28,11 +42,54 @@ public class ArmCalibration extends CommandBase {
         this._subsystem = subsystem;
         this._leader = subsystem.getLeader();
         this._controller = controller;
+
+        this._leader.configAllSettings(_leaderConfig);
+    }
+
+    public void addToFeedforward(String gain, double value) {
+        gain.toLowerCase();
+        switch (gain) {
+            case "ks":
+                ks = value;
+                break;
+            case "kv":
+                kv = value;
+                break;
+            case "kg":
+                kg = value;
+                break;
+            case "ka":
+                ka = value;
+                break;
+
+        }
+        _feedforward = new ArmFeedforward(ks, kv, kg, ka);
+    }
+
+    public void addToPID(String gain, double value) {
+        gain.toLowerCase();
+        switch (gain) {
+            case "kp":
+                _leaderConfig.slot0.kP = value;
+                break;
+            case "ki":
+                _leaderConfig.slot1.kI = value;
+                break;
+            case "kd":
+                _leaderConfig.slot2.kD = value;
+                break;
+        }
     }
 
     public void init() {
         SmartDashboard.putBoolean("is calibration activated", false);
         SmartDashboard.putNumber("arm voltage calibration", 0);
+
+        SmartDashboard.putString("pid new gain", "new gain");
+        SmartDashboard.putNumber("pid new value", 00);
+        SmartDashboard.putData("add to pid", new InstantCommand(() -> addToPID(
+                SmartDashboard.getString("pid new gain", "new gain"),
+                SmartDashboard.getNumber("pid new value", 00))));
     }
 
     public void execute() {
