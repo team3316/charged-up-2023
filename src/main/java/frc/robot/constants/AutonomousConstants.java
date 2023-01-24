c2692c5ed3f3ba88c8b47fa756692a667de7e99c
--global <51059131+DarkMetalMouse@users.noreply.github.com>
2023-01-24 19:47:33 +0200

    fixed robot size in PP

--- src/main/java/frc/robot/constants/AutonomousConstants.java
+++ src/main/java/frc/robot/constants/AutonomousConstants.java
@@ -1,7 +1,6 @@
 package frc.robot.constants;
 
 public class AutonomousConstants {
-    // TODO: update .pathplanner/settings.json
     // TODO: update speeds
     public static final double kMaxSpeedMetersPerSecond = 2.0;
     public static final double kMaxAccelerationMetersPerSecondSquared = 2.0;
