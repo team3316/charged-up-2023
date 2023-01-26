bd1ec9ca04087f0d81fc1a0cdcdff46829088afc
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-01-26 17:42:48 +0200

    added intake and eject

--- src/main/java/frc/robot/autonomous/AutoFactory.java
+++ src/main/java/frc/robot/autonomous/AutoFactory.java
@@ -10,8 +10,12 @@ import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import frc.robot.constants.AutonomousConstants;
 import frc.robot.constants.DrivetrainConstants;
+import frc.robot.subsystems.AutoRollerGripper;
+import frc.robot.subsystems.AutoRollerGripper.FolderState;
+import frc.robot.subsystems.AutoRollerGripper.RollersState;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
 /**
@@ -23,7 +27,7 @@ public class AutoFactory {
 
     private HashMap<String, Command> _eventMap = new HashMap<>();
 
-    public AutoFactory(Drivetrain drivetrain) {
+    public AutoFactory(Drivetrain drivetrain, AutoRollerGripper autoRollerGripper) {
         _autoBuilder = new SwerveAutoBuilder(
                 drivetrain::getPose,
                 drivetrain::resetPose,
@@ -39,9 +43,17 @@ public class AutoFactory {
         // for example:
         // _eventMap.put("marker1", new PrintCommand("Passed marker 1"));
         // _eventMap.put("intakeDown", new IntakeDown());
+        _eventMap.put("intake",
+                autoRollerGripper.getFoldCommand(FolderState.OUT) // nothing will happen if it's in the correct state
+                        .andThen(autoRollerGripper.getIntakeCommand()));
+
+        _eventMap.put("eject",
+                autoRollerGripper.getFoldCommand(FolderState.OUT) // for first eject
+                        .andThen(autoRollerGripper.getEjectCommand()));
+
     }
 
-    public CommandBase createAuto(Drivetrain drivetrain, String pathName) {
+    public CommandBase createAuto(String pathName) {
         PathPlannerTrajectory path = PathPlanner.loadPath(pathName,
                 new PathConstraints(AutonomousConstants.kMaxSpeedMetersPerSecond,
                         AutonomousConstants.kMaxAccelerationMetersPerSecondSquared));
