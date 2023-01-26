a749e6a41f1226d90eb929f53967d81039b122df
jizelda <70440778+jizelda@users.noreply.github.com>
2023-01-26 21:14:46 +0200

    clean up LimeLight Constants

--- src/main/java/frc/robot/constants/LimelightConstants.java
+++ src/main/java/frc/robot/constants/LimelightConstants.java
@@ -9,17 +9,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
  * LimelightConstants
  */
 public class LimelightConstants {
-    public static final double limelightHeightMeters = 0.27;
-    public static final double lowGoalTargetHeightMeters = 0.62;
-    public static final double highGoalTargetHeightMeters = 0;
-
-    public static final double limeLightGoalHeightOffset = 0.38;
-    public static final double limelightAngleFromFloorDegs = 20;
-
-    public static final double installDistanceFromLowGoalMeters = 0.78;
-
     public static final Rotation2d installAngle = new Rotation2d(0);
-
-    public static final List<String> actionEvents = Arrays.asList(new String[] { "arm", "manipulator" });
 
 }
