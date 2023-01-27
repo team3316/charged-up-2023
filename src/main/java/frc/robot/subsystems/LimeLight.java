9a3052a70210f95e2ee20007b504ab99a69b25e5
jizelda <70440778+jizelda@users.noreply.github.com>
2023-01-27 14:56:38 +0200

    added limelight quick-death protection

--- src/main/java/frc/robot/subsystems/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -13,21 +13,37 @@ public class LimeLight extends SubsystemBase {
     NetworkTable limeLightTable;
     private NetworkTableEntry tx;
     private NetworkTableEntry ty;
+    private NetworkTableEntry hasTarget;
+    private double lastX;
+    private double lastY;
 
     /** Creates a new LimeLight. */
     public LimeLight() { // CR: add a way to send the config to the limelight trough code
         limeLightTable = NetworkTableInstance.getDefault().getTable("limelight");
         tx = limeLightTable.getEntry("tx");
         ty = limeLightTable.getEntry("ty");
+        hasTarget = limeLightTable.getEntry("tv");
+    }
+
+    public boolean hasTarget() {
+        return hasTarget.getDouble(0) == 1;
     }
 
     public double getXAngle() {
-        return -1 * tx.getDouble(0); // negative because LL and pigeon have opposite sensor fazzes
+        if (this.hasTarget()) {
+            lastX = -1 * tx.getDouble(0);
+        }
+
+        return lastX; // negative because LL and pigeon have opposite sensor fazzes
     }
 
     // height dif between goal and limelight devided by tan of limelight y angle.
     public double getYAngle() {
-        return ty.getDouble(0);
+        if (this.hasTarget()) {
+            lastY = ty.getDouble(0);
+        }
+
+        return lastY;
     }
 
 }
