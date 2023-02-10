4fe989f656656936acc28f7559b30f5dae279af1
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-02-10 17:13:28 +0200

    deleted auto factory logging

--- src/main/java/frc/robot/autonomous/AutoFactory.java
+++ src/main/java/frc/robot/autonomous/AutoFactory.java
@@ -11,16 +11,9 @@ import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
 import edu.wpi.first.util.datalog.DataLog;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.constants.AutonomousConstants;
 import frc.robot.constants.DrivetrainConstants;
 import frc.robot.subsystems.drivetrain.Drivetrain;
@@ -33,8 +26,6 @@ public class AutoFactory {
     private SwerveAutoBuilder _autoBuilder;
 
     private HashMap<String, Command> _eventMap = new HashMap<>();
-
-    private DoubleLogEntry _logX, _logY, _logR;
 
     public AutoFactory(Drivetrain drivetrain, DataLog log) {
         _autoBuilder = new SwerveAutoBuilder(
@@ -58,23 +49,11 @@ public class AutoFactory {
     }
 
     public CommandBase createAuto(String pathName) {
-        Timer time = new Timer();
         List<PathPlannerTrajectory> path = PathPlanner.loadPathGroup(pathName,
                 new PathConstraints(AutonomousConstants.kMaxSpeedMetersPerSecond,
                         AutonomousConstants.kMaxAccelerationMetersPerSecondSquared));
-        return _autoBuilder.fullAuto(path).alongWith(new FunctionalCommand(
-                () -> time.start(),
-                () -> {
-                    this._logX.append(path.get(0).sample(time.get()).poseMeters.getX());
-                    this._logY.append(path.get(0).sample(time.get()).poseMeters.getY());
-                    this._logR.append(path.get(0).sample(time.get()).poseMeters.getRotation().getDegrees());
-                },
-                (interrupted) -> {
-                    time.stop();
-                    time.reset();
-                },
-                () -> false));
-            }
+        return _autoBuilder.fullAuto(path);
+    }
 
     public CommandBase createfollow(PathPlannerTrajectory path) {
         return _autoBuilder.followPath(path);
