e6c6217136974693f36e168184558d098909a9a3
jizelda <70440778+jizelda@users.noreply.github.com>
2023-02-11 14:36:37 +0200

    changed some constatnts

--- src/main/java/frc/robot/constants/RollerGripperConstants.java
+++ src/main/java/frc/robot/constants/RollerGripperConstants.java
@@ -8,15 +8,14 @@ public class RollerGripperConstants {
     public final static int rollerLimitSwitchPort = 1;
 
     // TODO: calibrate values when system is built
-    public final static double rollerIntakePercent = 0.3; // percent
-    public final static double rollerEjectPercent = -0.3; // percent
+    public final static double rollerIntakePercent = +0.7; // percent
+    public final static double rollerEjectPercent = -0.7; // percent
     public final static double rollerOffPercent = 0; // percent
 
     public final static double ejectSleepDurationSeconds = 0.5; // seconds
-    public final static double intakeSleepDurationSeconds = 0.5; // seconds
+    public final static double intakeSleepDurationSeconds = 0.15; // seconds
 
     public final static double ejectDurationSeconds = 0.02; // seconds
-    
 
     public final static int solenoidForwardChannel = 4;
     public final static int solenoidReverseChannel = 5;
