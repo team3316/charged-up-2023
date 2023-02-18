2d9696092a163b7c9ff354c4d6fd239c0d1481b2
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-02-18 20:14:39 +0200

    added logic to superStructure

--- src/main/java/frc/robot/subsystems/ArmFunnelSuperStructure.java
+++ src/main/java/frc/robot/subsystems/ArmFunnelSuperStructure.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Arm.ArmState;
 import frc.robot.subsystems.Funnel.FunnelPosition;
@@ -15,6 +16,41 @@ public class ArmFunnelSuperStructure {
     }
 
     public CommandBase getSetStateCommand(ArmState arm, FunnelPosition funnel) {
-        return new InstantCommand();
+        CommandBase armMovementSequence = new InstantCommand();
+        if (arm == m_arm.getTargetState()) {
+            switch (arm) {
+                case COLLECT:
+                    armMovementSequence = Commands.sequence(
+                            m_arm.getSetStateCommand(ArmState.DRIVE),
+                            m_funnel.setFunnelPositionCommand(FunnelPosition.OPEN),
+                            m_arm.getSetStateCommand(ArmState.COLLECT));
+                    break;
+
+                case DRIVE:
+                    if (m_arm.getTargetState() == ArmState.COLLECT)
+                        armMovementSequence = Commands.sequence(
+                                m_funnel.setFunnelPositionCommand(FunnelPosition.OPEN),
+                                m_arm.getSetStateCommand(ArmState.DRIVE));
+                    break;
+
+                default:
+                    if (m_arm.getTargetState() == ArmState.COLLECT) {
+                        armMovementSequence = Commands.sequence(
+                                m_funnel.setFunnelPositionCommand(FunnelPosition.OPEN),
+                                m_arm.getSetStateCommand(ArmState.DRIVE),
+                                m_funnel.setFunnelPositionCommand(FunnelPosition.CLOSED),
+                                m_arm.getSetStateCommand(arm));
+                    } else
+                        armMovementSequence = Commands.sequence(
+                                m_funnel.setFunnelPositionCommand(FunnelPosition.CLOSED),
+                                m_arm.getSetStateCommand(arm));
+
+            }
+        }
+
+        if (funnel != m_funnel.getFunnelPosition())
+            return Commands.sequence(armMovementSequence, m_funnel.setFunnelPositionCommand(funnel));
+        return armMovementSequence;
+
     }
 }
