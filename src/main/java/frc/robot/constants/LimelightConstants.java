1d66348e1b5899214d4717a9231753312ed08644
jizelda <70440778+jizelda@users.noreply.github.com>
2023-02-23 11:58:51 +0200

    fixed an errore in limelight constatnts

--- src/main/java/frc/robot/constants/LimelightConstants.java
+++ src/main/java/frc/robot/constants/LimelightConstants.java
@@ -10,11 +10,11 @@ public class LimelightConstants {
     public static final double AlignLimeLightToPigeonPhase = -1;
 
     public static final PIDFGains xGainsRetro = new PIDFGains(0.2, 0.0, 0.01, 0.0);
-    public static final PIDFGains xGainsApril = new PIDFGains(0.05, 0.0, 0.01, 0.0);
+    public static final PIDFGains xGainsApril = new PIDFGains(0.1, 0.0, 0, 0.0);
     public static final double xTol = 1;
 
     public static final PIDFGains yGainsRetro = new PIDFGains(0.1, 0.0, 0.0, 0.0);
-    public static final PIDFGains yGainsApril = new PIDFGains(0.05, 0.0, 0.0, 0.0);
+    public static final PIDFGains yGainsApril = new PIDFGains(0.1, 0.0, 0.0, 0.0);
     public static final double yTol = 0.5;
 
     public static final PIDFGains thetaGains = new PIDFGains(0.1, 0.0, 0.0, 0.0);
