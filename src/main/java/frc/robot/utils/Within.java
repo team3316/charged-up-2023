0e7d7a2323a04f4dfcc533b0ab713470564f3c7d
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-02-28 22:07:32 +0200

    removed unused class

--- src/main/java/frc/robot/utils/Within.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.utils;
-
-public class Within {
-    public static boolean range(double value, double target, double tol) {
-        return Math.abs(target - value) <= tol;
-    }
-}
