cc0b173e34d95ac4e6fb921b04cf43e3d21f9303
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-03-04 18:30:28 +0200

    implemented option: turn on LEDs only on alignement.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -104,9 +104,11 @@ public class RobotContainer {
 
         /* align with vision target */
         _driverController.cross()
-                .whileTrue(new InstantCommand(() -> m_drivetrain.restartControllers(), m_drivetrain).andThen(
-                        new RunCommand(() -> m_drivetrain.driveByVisionControllers(m_limeLight.getFieldTX(),
-                                m_limeLight.getFieldTY()), m_drivetrain)));
+                .whileTrue(new InstantCommand(() -> m_drivetrain.restartControllers(), m_drivetrain)
+                        .alongWith(new InstantCommand(() -> m_limeLight.setLED(true), m_limeLight)).andThen(
+                                new RunCommand(() -> m_drivetrain.driveByVisionControllers(m_limeLight.getFieldTX(),
+                                        m_limeLight.getFieldTY()), m_drivetrain))
+                        .finallyDo((interrupted) -> m_limeLight.setLED(false)));
 
         /* Operator triggers */
         // Collect sequence
@@ -170,6 +172,7 @@ public class RobotContainer {
     private void setCubeInternalState() {
         this._scoreMidCube = true;
         m_limeLight.setPipeLine(LimelightConstants.pipeLineAprilTags);
+        m_limeLight.setLED(false);
         SmartDashboard.putBoolean("target GP", this._scoreMidCube);
         m_drivetrain.setVisionAprilPID();
         m_manipulator.setIRSensorState(IRSensorState.CUBE);
@@ -178,6 +181,7 @@ public class RobotContainer {
     private void setConeInternalState() {
         this._scoreMidCube = false;
         m_limeLight.setPipeLine(LimelightConstants.pipeLineRetroReflective);
+        m_limeLight.setLED(false);
         SmartDashboard.putBoolean("target GP", this._scoreMidCube);
         m_drivetrain.setVisionRetroPID();
         m_manipulator.setIRSensorState(IRSensorState.CONE);
