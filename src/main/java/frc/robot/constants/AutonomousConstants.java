0d6fc9be369357765268bea9c73a5862b36547e0
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-03-10 10:15:54 +0200

    updated auto constants

--- src/main/java/frc/robot/constants/AutonomousConstants.java
+++ src/main/java/frc/robot/constants/AutonomousConstants.java
@@ -2,14 +2,14 @@ package frc.robot.constants;
 
 public class AutonomousConstants {
     // TODO: update speeds
-    public static final double kMaxSpeedMetersPerSecond = 2.0;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 3.0;
+    public static final double kMaxSpeedMetersPerSecond = 2;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 2;
 
     public static final String defaultPath = "bot-3-gp-engage";
 
     // TODO: update P gains
-    public static final double kPTranslationController = 1;
-    public static final double kPThetaController = 1.5;
+    public static final double kPTranslationController = 3;
+    public static final double kPThetaController = 3;
 
     public static final double engageSpeedMetersPerSecond = 0.4;
 }
