bb9f23aab8c3bd1cdcd2bd5bd4f7432f6ea78da3
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-03-17 17:30:06 +0200

    update threshold

--- src/main/java/frc/robot/constants/SSDetectorConstants.java
+++ src/main/java/frc/robot/constants/SSDetectorConstants.java
@@ -2,5 +2,5 @@ package frc.robot.constants;
 
 public class SSDetectorConstants {
     public final static int id = 1;
-    public final static double threshold = 0.2;
+    public final static double threshold = 0.1;
 }
