01ced463e9e873a8262c821ea09a403154a0011d
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-03-17 14:41:00 +0200

    updated all IR values according to tests

--- src/main/java/frc/robot/constants/ManipulatorConstants.java
+++ src/main/java/frc/robot/constants/ManipulatorConstants.java
@@ -14,13 +14,17 @@ public class ManipulatorConstants {
 
     /*
      * No game piece, funnel open = 0.05
-     * No game piece, funnel closed = 0.10
+     * No game piece, funnel closed = 0.075
+     * cube, open = 0.35-0.48
+     * cube, closed = 0.45-0.6
+     * cone, open = 0.21-0.26
+     * cone, closed = 0.3
      */
     // ir constants
     public static final double FunnelingDetectorThreshold = 0.12;
     public static final double FunnelingDetectorHysteresis = 0.0;
-    public static final double CONEDetectorThreshold = 0.26;
-    public static final double CONEDetectorHysteresis = 0.1;
-    public static final double CUBEDetectorThreshold = 0.32;
-    public static final double CUBEDetectorHysteresis = 0.1;
+    public static final double CONEDetectorThreshold = 0.20;
+    public static final double CONEDetectorHysteresis = 0.0;
+    public static final double CUBEDetectorThreshold = 0.35;
+    public static final double CUBEDetectorHysteresis = 0.0;
 }
