5f36fe5a253cb2be682b108fd4ec655ac0f18089
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-03-19 19:13:42 +0200

    made it so funnel will be open after reject

--- src/main/java/frc/robot/subsystems/ArmFunnelSuperStructure.java
+++ src/main/java/frc/robot/subsystems/ArmFunnelSuperStructure.java
@@ -46,7 +46,7 @@ public class ArmFunnelSuperStructure {
          * 
          */
 
-        if (wantedArmState != ArmState.COLLECT || wantedArmState != ArmState.REJECT)
+        if (wantedArmState != ArmState.COLLECT && wantedArmState != ArmState.REJECT)
             wantedFunnelState = FunnelState.CLOSED;
 
         if (wantedArmState == m_arm.getTargetState()) {
@@ -68,7 +68,7 @@ public class ArmFunnelSuperStructure {
                     Commands.deadline(
                             m_arm.getSetStateCommand(wantedArmState, ArmTrapProfile.FAST),
                             new WaitUntilCommand(() -> m_arm.getAngle() > ArmConstants.outOfFunnelAngle)
-                                    .andThen(m_funnel.setFunnelStateCommand(FunnelState.CLOSED))));
+                                    .andThen(m_funnel.setFunnelStateCommand(wantedFunnelState))));
 
         if (wantedArmState == ArmState.COLLECT)
             return Commands.sequence(
