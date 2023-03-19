a887c2508e6d83dd973b0d89ca9d779ab525d23b
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-03-19 18:48:17 +0200

    removed d from x-retro PID

--- src/main/java/frc/robot/constants/LimelightConstants.java
+++ src/main/java/frc/robot/constants/LimelightConstants.java
@@ -9,7 +9,7 @@ import frc.robot.motors.PIDFGains;
 public class LimelightConstants {
     public static final double AlignLimeLightToPigeonPhase = -1;
 
-    public static final PIDFGains xGainsRetro = new PIDFGains(0.1, 0.0, 0.01, 0.0);
+    public static final PIDFGains xGainsRetro = new PIDFGains(0.1, 0.0, 0, 0.0);
     public static final PIDFGains xGainsApril = new PIDFGains(0.1, 0.0, 0, 0.0);
     public static final double xTol = 1;
 
