71c3e451f9d5f849ff667428c6dfe78f03ea9c58
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-03-22 11:02:44 +0200

    calibrated sss IR sensor

--- src/main/java/frc/robot/constants/SSDetectorConstants.java
+++ src/main/java/frc/robot/constants/SSDetectorConstants.java
@@ -2,5 +2,5 @@ package frc.robot.constants;
 
 public class SSDetectorConstants {
     public final static int id = 1;
-    public final static double threshold = 0.1;
+    public final static double threshold = 0.11;
 }
--- src/main/java/frc/robot/subsystems/SSDetector.java
+++ src/main/java/frc/robot/subsystems/SSDetector.java
@@ -1,13 +1,17 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.RollerGripperConstants;
 import frc.robot.constants.SSDetectorConstants;
 
 public class SSDetector extends SubsystemBase {
     private static final double ADC_RESOLUTION = 4096; // 12 bits
     private AnalogInput _detector;
+
+    private Debouncer _debouncer = new Debouncer(0.1);
 
     public SSDetector() {
         _detector = new AnalogInput(SSDetectorConstants.id);
