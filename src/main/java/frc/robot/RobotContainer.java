0e7296b3853460ae455a82311e93301c659d0367
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2023-10-02 12:24:52 +0300

    cleaned up sideConeSequence for testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -265,18 +265,15 @@ public class RobotContainer {
 
     private CommandBase getSideConeSequence() {
         return Commands.sequence(
-                new InstantCommand(() -> setCubeInternalState()), // TODO:chck if CneeInternalState works
+                m_ArmFunnelSuperStructure.getSetStateCommand(ArmState.COLLECT, FunnelState.COLLECT),
                 m_manipulator.setManipulatorStateCommand(ManipulatorState.OPEN),
                 new WaitCommand(1),
-                m_manipulator.setManipulatorStateCommand(ManipulatorState.HOLD),
                 new WaitCommand(1),
-                getCollectSequence().withTimeout(1),
-                Commands.sequence(m_manipulator.setManipulatorStateCommand(ManipulatorState.OPEN),
-                        m_ArmFunnelSuperStructure.getSetStateCommand(ArmState.COLLECT, FunnelState.READJUST),
-                        new WaitCommand(0.5),
-                        m_ArmFunnelSuperStructure.getSetStateCommand(ArmState.COLLECT, FunnelState.CLOSED),
-                        m_manipulator.setManipulatorStateCommand(ManipulatorState.HOLD)),
-                m_ArmFunnelSuperStructure.getSetStateCommand(ArmState.MID_CONE, FunnelState.OPEN));
+                getCollectSequence(),
+                m_manipulator.setManipulatorStateCommand(ManipulatorState.HOLD),
+                m_ArmFunnelSuperStructure.getSetStateCommand(ArmState.MID_CONE, FunnelState.OPEN),
+                new WaitCommand(1),
+                m_manipulator.setManipulatorStateCommand(ManipulatorState.OPEN));
     }
 
     private CommandBase getEngageSequence() {
